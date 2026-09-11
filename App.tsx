68e4f1342ca5d530bc48874314c89ae8b907fa60
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-09-10 21:09:29 -0500

    BN: sobreescribiendo App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DrawerNavigator from './src/navigators/DrawerNavigator';
+import ChestDetailScreen from './src/screens/ChestDetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Drawer" component={DrawerNavigator} />
+        <Stack.Screen name="ChestDetailScreen" component={ChestDetailScreen} options={{ headerShown: true, title: 'Detalle' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
